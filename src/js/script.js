333baacd227c3c9b826c88dcc13d747357a66a07
jaber-2004 <md9292463@gmail.com>
2025-09-27 12:00:15 +0600

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+// Counter Part Start
+jQuery(document).ready(function ($) {
+  $(".counter").counterUp({
+    delay: 10,
+    time: 3000,
+  });
+});
+// Counter Part End
+
+// AOS Animate Start
+AOS.init();
+// AOS Animate End
